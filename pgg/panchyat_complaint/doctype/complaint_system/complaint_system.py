b9e3ada6d0501b41c7eeabad5e9cbd558e091136
sagar30051991 <sagarshiragawakar@gmail.com>
2019-01-25 00:05:05 +0530

    Add file Upload method
    Add file Upload method

--- pgg/panchyat_complaint/doctype/complaint_system/complaint_system.py
+++ pgg/panchyat_complaint/doctype/complaint_system/complaint_system.py
@@ -5,6 +5,15 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.handler import uploadfile
 
 class ComplaintSystem(Document):
 	pass
+
+@frappe.whitelist()
+def attach_file_to_doc():
+    file_upload_response = uploadfile()
+    if file_upload_response:
+        return file_upload_response
+    else:
+        frappe.throw('Unable to upload')
